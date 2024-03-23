78314dfc1e8d571992c0a0637000706d941ebce0
Akraja712 <ashokkumar7hitter@gmail.com>
2024-03-23 20:22:59 +0530

    home.php

--- home.php
+++ home.php
@@ -31,7 +31,7 @@ include "header.php";
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Pocket Farm- Dashboard</title>
+    <title>Trusted Apps- Dashboard</title>
 </head>
 
 <body>
